8cb886d6a80549651abdccb9441f18265ac3b776
herzaso <herzaso@gmail.com>
2017-07-03 10:51:27 +0300

    fix get files parameter dynamically

--- googledrive/__init__.py
+++ googledrive/__init__.py
@@ -7,4 +7,5 @@ Stream = source.GoogleDrive
 def get_files(source, *args, **kwargs):
     return Stream(source, *args, **kwargs).get_files()
 
-CONFIG['params'][-1]['values'] = get_files
+files_param = [x for x in CONFIG['params'] if x['name'] == 'files'][0]
+files_param['values'] = get_files
